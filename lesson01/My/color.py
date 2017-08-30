33cb648da4c4ebb210c19a014946059b87d79bbd
user674 <rydovoi50@rambler.ru>
2017-08-30 13:32:40 +0300

    add

---
+++ lesson01/My/color.py
@@ -0,0 +1,9 @@
+color = 'green'
+if color == 'blue':
+    print ('синий')
+elif color == 'red':
+    print ('красный')
+if color == 'green':
+    print ('зеленый')
+else:
+    print ('незвестный цвет')
