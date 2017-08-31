358cb32623e67da4125bb5c0e95dc17cd505d95a
user674 <rydovoi50@rambler.ru>
2017-08-31 07:58:52 +0300

    04_easy

--- lesson01/My/color.py
+++
@@ -1,9 +0,0 @@
-color = 'green'
-if color == 'blue':
-    print ('синий')
-elif color == 'red':
-    print ('красный')
-if color == 'green':
-    print ('зеленый')
-else:
-    print ('незвестный цвет')
