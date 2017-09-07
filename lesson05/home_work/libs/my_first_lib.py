fc6b6fc27c8350d478495b966eb7ef8fc09d1025
user674 <rydovoi50@rambler.ru>
2017-09-07 22:17:13 +0300

    Lesson_5

---
+++ lesson05/home_work/libs/my_first_lib.py
@@ -0,0 +1,4 @@
+def first():
+    return 'какая то надпись'
+def two(num_1, num_2):
+    return num_1 * num_2
---
+++ lesson05/home_work/ryn.py
@@ -0,0 +1,3 @@
+import libs.my_first_lib as my_lib
+print(my_lib.first())
+print(my_lib.two(6, 7))
