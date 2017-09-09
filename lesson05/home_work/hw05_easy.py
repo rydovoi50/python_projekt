46c082b6786b0f4ae185cdb985857fc96d0fbcd6
user674 <rydovoi50@rambler.ru>
2017-09-09 12:14:45 +0300

    completed_hw05_easy

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,40 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+
+import os
+def create_a_folder():
+    for i in range(1, 10):
+        try:
+            os.mkdir('dir_' + str(i))
+        except FileExistsError:
+            print('Папка {} в директории {} уже существует'.format('dir_' + str(i), os.getcwd()))
+
+def delete_folder():
+    for i in range(1, 10):
+        os.rmdir('dir_' + str(i))
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+
+def list():
+    print(os.listdir())
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def cope_file():
+    with open('hw05_easy.py', 'r', encoding='UTF-8') as hw:
+        file_1 = hw.read()
+    with open('new_hw05_easy.py', 'w', encoding='UTF-8') as new:
+        new.write(file_1)
+
+
+list()
+create_a_folder()
+list()
+delete_folder()
+list()
+cope_file()
+list()
