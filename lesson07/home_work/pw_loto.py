a23af1f38025ce3a85346fe10785f8dbf7ce513b
Zver <rudovoi50@rambler.ru>
2017-09-25 21:34:54 +0300

    loto

--- lesson07/home_work/pw_loto.py
+++ lesson07/home_work/pw_loto.py
@@ -3,29 +3,50 @@ import random
 
 class Loto:
 
+    def __init__(self):
+        self.ls_keg = [x for x in range(1, 91)]
+        random.shuffle(self.ls_keg)
+        self.keg()
+        self.card('Пользователь')
+        self.card('Компьютер')
+
+    def keg(self):
+        print(self.ls_keg.pop())
+        print('осталось {} бочонков'.format(len(self.ls_keg)))
+
     def card(self, ad):
+        self.ad = ad
         num = random.sample(range(1, 91), 15)
-        a = sorted(num[0: 5]) + list(' ' * 4)
-        b = sorted(num[5: 10]) + list(' ' * 4)
-        c = sorted(num[10:]) + list(' ' * 4)
-        random.shuffle(a)
-        random.shuffle(b)
-        random.shuffle(c)
-        return '{:-^41} \n{} \n{} \n{} \n{:-^41}'.format(ad, a, b, c, '')
+        a = sorted(num[0: 5])  # + ['--' for x in range(4)]
+        b = sorted(num[5: 10])  # + ['--' for x in range(4)]
+        c = sorted(num[10:])  # + ['--' for x in range(4)]
+        x = [self.ad, self.ls(a), self.ls(b), self.ls(c), '']
+        print('{:-^26}\n{}\n{}\n{}\n{:-^26}'.format(*x))
+
+    def ls(self, ls):
+        for i in range(0, 4):
+            _ = random.randint(0, 9)
+            ls.insert(_, '--')
+        y = [str(x) for x in ls]
+        for x in y:
+            if len(x) == 1:
+                z = y.index(x)
+                x = x + ' '
+                y[z] = x
+        return ' '.join(y)
+
+    def y_n(self):
+
 
 numb = Loto()
-print(numb.card('Пользователь'))
-print(numb.card('Компьютер'))
-
-a = []
-b = random.sample(range(1, 91), 15)
-d = '----------'
-for i in range(0, 9):
-    a.append(b[i])
-    a.append('--')
-    # print(a)
-
-random.shuffle(a)
-# print(a)
-
+# print(numb.card('Пользователь'))
+# print(numb.card('Компьютер'))
+#
+# a = []
+# b = random.sample(range(1, 91), 15)
+# d = '----------'
+# for i in range(0, 9):
+#     a.append(b[i])
+#     a.append('--')
+#     # print(a)
 
